c30d9dfc7b487fa1b2052542256493d9991b1c83
12Ferdian12 <Ferdianadinata07@gmail.com>
2023-05-16 20:25:47 +0700

    UPDATE: -ADDED CONTACT(NEED FIXIN) -ADDED FOOTER

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./Views/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        'Japan': ["'Noto Sans JP', sans-serif"]
+      }
     },
   },
   plugins: [],
